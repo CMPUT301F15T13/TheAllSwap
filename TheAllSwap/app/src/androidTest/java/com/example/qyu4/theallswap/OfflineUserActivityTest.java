f0ed8025dd3d2807787b9072ff0ca302975eda4e
AlanFor301 <allendiabolo@gmail.com>
2015-10-07 10:49:25 -0600

    inital code commit with Offline user activity test skeleton done

---
+++ TheAllSwap/app/src/androidTest/java/com/example/qyu4/theallswap/OfflineUserActivityTest.java
@@ -0,0 +1,31 @@
+package com.example.qyu4.theallswap;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by qyu4 on 10/7/15.
+ * this is the test file for testing user offline activities.
+ * At the date Oct. 7th, 2015, this file is just a skeleton testfile and it does not
+ * have it real functionality.
+ */
+public class OfflineUserActivityTest extends ActivityInstrumentationTestCase2 {
+    public OfflineUserActivityTest(){
+        super(UserEditInventoryActivity.class);
+    }
+    public void testNewItemCreated(){
+        assertTrue(testIsOffline());
+        //TODO: creating item offline;
+        //TODO: reconnect internet;
+        assertTrue(testIsOnline());
+        //TODO: get result while online
+        //TODO: check if new result and item created when offline is equal.
+
+    }
+    public boolean testIsOffline(){
+        return true;
+    }
+    public boolean testIsOnline(){
+        return true;
+    }
+
+}
---
+++ TheAllSwap/app/src/androidTest/java/com/example/qyu4/theallswap/UserOfflineTradingTest.java
@@ -0,0 +1,13 @@
+package com.example.qyu4.theallswap;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by qyu4 on 10/7/15.
+ */
+public class UserOfflineTradingTest extends ActivityInstrumentationTestCase2 {
+    UserOfflineTradingTest(){
+        super(UserTradingActivity.class);
+    }
+
+}
