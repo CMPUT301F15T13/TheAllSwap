548c90b633f49e9d865bf691f08a58037068301e
AlanFor301 <allendiabolo@gmail.com>
2015-10-14 22:11:57 -0600

    Update OfflineUserActivityTest.java
    sample test edit

--- TheAllSwap/app/src/androidTest/java/com/example/qyu4/theallswap/OfflineUserActivityTest.java
+++ TheAllSwap/app/src/androidTest/java/com/example/qyu4/theallswap/OfflineUserActivityTest.java
@@ -13,13 +13,16 @@ public class OfflineUserActivityTest extends ActivityInstrumentationTestCase2 {
         super(UserEditInventoryActivity.class);
     }
     public void testNewItemCreated(){
+        ArrayList<Item> itemList = new ArrayList<Item>();
         assertTrue(testIsOffline());
         //TODO: creating item offline;
+        Item itemOffline = new Item("offline item", "category", "offline item description");
+        itemList.add(itemOffline);
         //TODO: reconnect internet;
         assertTrue(testIsOnline());
         //TODO: get result while online
         //TODO: check if new result and item created when offline is equal.
-
+        assertEqual("offline item", item.getName(0));
     }
     public void testAddItem(){
         //TODO: initialize a item name(string Type: itemName)
@@ -27,9 +30,11 @@ public class OfflineUserActivityTest extends ActivityInstrumentationTestCase2 {
         //assertEquals(item.getName().equals(String));
     }
     public boolean testIsOffline(){
-        return true;
+        boolean cantConnect = NetworkConnector.tryConnect(); 
+        return not canConnect;
     }
     public boolean testIsOnline(){
+        boolean canConnect = NetworkConnector.tryConnect(); 
         return true;
     }
 
