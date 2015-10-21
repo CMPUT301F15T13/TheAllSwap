0953b6fdb250518438f88753a5050a3eaea8af05
AlanFor301 <allendiabolo@gmail.com>
2015-10-21 10:52:31 -0600

    start working on View Controller part

--- TheAllSwap/app/src/main/java/com/example/qyu4/theallswap/View/LaunchingPage.java
+++ TheAllSwap/app/src/main/java/com/example/qyu4/theallswap/View/LaunchingPage.java
@@ -1,40 +1,38 @@
 package com.example.qyu4.theallswap.View;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import com.example.qyu4.theallswap.R;
 
-public class LaunchingPage extends Activity {
-
+public class LaunchingPage extends Activity implements OnClickListener {
+    private LaunchingPage activity = this;
+    private Button buttonToLoginPage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launching_page);
+        buttonToLoginPage = (Button) findViewById(R.id.b_to_login_page);
+        buttonToLoginPage.setOnClickListener(this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_launching_page, menu);
-        return true;
-    }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View view) {
+        if(view.getId()==R.id.b_to_login_page){
+            classIntent(UserLogin.class);
         }
-
-        return super.onOptionsItemSelected(item);
+    }
+    public void classIntent(Class newClass){
+        Intent openNewActivity = new Intent(activity, newClass);
+        startActivity(openNewActivity);
     }
 }
