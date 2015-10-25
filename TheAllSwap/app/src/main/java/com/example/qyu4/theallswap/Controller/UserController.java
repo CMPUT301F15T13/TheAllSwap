e94e75ac034febd0ab56a1b6438509f8c0d73a91
AlanFor301 <allendiabolo@gmail.com>
2015-10-24 23:36:11 -0600

    add save file method to controller

--- TheAllSwap/app/src/main/java/com/example/qyu4/theallswap/Controller/UserController.java
+++ TheAllSwap/app/src/main/java/com/example/qyu4/theallswap/Controller/UserController.java
@@ -5,6 +5,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Objects;
+
 /**
  * Created by qyu4 on 10/24/15.
  */
@@ -21,5 +30,21 @@ public class UserController {
     public void makeToastIn(Context context){
         Toast.makeText(context, "invalid same password", Toast.LENGTH_LONG).show();
     }
+    private void saveInFile(String FileName, Context context, Objects nameOfClass) {
 
+            try {
+                FileOutputStream fos = context.openFileOutput(FileName, 0);
+                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+                Gson gson = new Gson();
+                gson.toJson(nameOfClass, out);
+                out.flush();
+                fos.close();
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                throw new RuntimeException(e);
+            }
+        }
 }
