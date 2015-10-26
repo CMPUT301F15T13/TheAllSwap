2a0f2c6e5f3dd7060cbddbe1410f4a96087cbea5
AlanFor301 <allendiabolo@gmail.com>
2015-10-25 21:32:30 -0600

    friend part done

--- TheAllSwap/app/src/main/java/com/example/qyu4/theallswap/View/UserFriends.java
+++ TheAllSwap/app/src/main/java/com/example/qyu4/theallswap/View/UserFriends.java
@@ -1,20 +1,61 @@
 package com.example.qyu4.theallswap.View;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.example.qyu4.theallswap.Controller.UserController;
+import com.example.qyu4.theallswap.Model.User;
 import com.example.qyu4.theallswap.R;
 
-public class UserFriends extends ActionBarActivity {
+import java.util.ArrayList;
 
+public class UserFriends extends Activity {
+    private UserFriends activity = this;
+    private UserController uc = new UserController();
+    private ArrayList<User> userList= new ArrayList<User>();
+    private ArrayAdapter<User> adapter;
+    private ListView friendList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_friends);
-    }
+        friendList = (ListView)findViewById(R.id.lv_all_friend);
+        friendList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //TODO: some stuff
+                uc.makeInvalidPasswordToast(activity);
+            }
 
+
+        });
+    }
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        //loadFromFile();oldTweetsList.setAdapter(adapter);
+        /***************************************************
+         TODO: add loading friends list method.
+         *************************************************/
+        User sampleUser = new User();
+        sampleUser.setUserId("123");
+        sampleUser.setUserPassword("123");
+        userList.add(sampleUser);
+        /***************************************************
+         TODO: add loading friends list method.
+         *************************************************/
+
+        adapter = new ArrayAdapter<User>(this, R.layout.list_item, userList);
+        friendList.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
