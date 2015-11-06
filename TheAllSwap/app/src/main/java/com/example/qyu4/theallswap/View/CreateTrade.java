4ae218d6964f30ef4d5e08f245fde3dbaa443182
Alexander Ozero <a.ozero@live.com>
2015-11-06 14:03:52 -0700

    Added more CreateTrade functionality

--- TheAllSwap/app/src/main/java/com/example/qyu4/theallswap/View/CreateTrade.java
+++ TheAllSwap/app/src/main/java/com/example/qyu4/theallswap/View/CreateTrade.java
@@ -1,20 +1,71 @@
 package com.example.qyu4.theallswap.View;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.qyu4.theallswap.Controller.UserController;
+import com.example.qyu4.theallswap.Model.User;
 import com.example.qyu4.theallswap.R;
 
-public class CreateTrade extends ActionBarActivity {
+import java.util.ArrayList;
 
+public class CreateTrade extends ActionBarActivity {
+    private CreateTrade activity = this;
+    private UserController uc = new UserController();
+    private ArrayList<User> userList= new ArrayList<User>();
+    private static final String FILENAME = "userProfile.txt";
+    private ArrayAdapter<User> adapter;
+    private ListView itemList;
+    private ListView itemList2;
+    private ArrayList resultList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_trade);
+        itemList = (ListView)findViewById(R.id.listView);
+        itemList2 = (ListView)findViewById(R.id.listView2);
 
+        itemList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = ((TextView) view).getText().toString();
+                Toast.makeText(getApplicationContext(), "Selected: " + selectedItem,
+                        Toast.LENGTH_SHORT).show();
 
+                //TODO: First item selected
+            }
+        });
+
+        itemList2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = ((TextView) view).getText().toString();
+                Toast.makeText(getApplicationContext(), "Selected: " + selectedItem,
+                        Toast.LENGTH_SHORT).show();
+                //TODO: Second item selected
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        userList = uc.loadUserFromFile(activity, FILENAME, userList);
+        User currentUser = userList.get(0);
+        resultList = uc.convertItemToString(currentUser, resultList);
+
+        // TODO? Use spinners instead of lists for clearer selection
+        adapter = new ArrayAdapter<User>(this, R.layout.list_item, resultList);
+        itemList.setAdapter(adapter);
+        itemList2.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 
     @Override
