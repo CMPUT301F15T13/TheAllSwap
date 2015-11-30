8aedd91d746e97b7add26368a49f5c954d1bf41a
jlx339 <jlx339@hotmail.com>
2015-11-29 23:49:24 -0700

    Update on UserInventoryWithImage

--- TheAllSwap/app/src/main/java/com/example/qyu4/theallswap/View/UserInventoryWithImage.java
+++ TheAllSwap/app/src/main/java/com/example/qyu4/theallswap/View/UserInventoryWithImage.java
@@ -19,23 +19,16 @@ import com.example.qyu4.theallswap.R;
 
 import java.util.ArrayList;
 
-public class UserInventoryWithImage extends ListActivity {
+public class UserInventoryWithImage extends ActionBarActivity {
     private UserInventoryWithImage activity = this;
     private UserController uc = new UserController();
 
     private UserList userList;
     private User currentUser;
 
-    private ArrayAdapter<String> adapter;
+    private ArrayAdapter<User> adapter;
     private ListView itemList;
     private ArrayList<String> resultList = new ArrayList<>();
-
-
-    private String[] itemname = {
-            "111",
-            "222",
-            "333"
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,15 +39,37 @@ public class UserInventoryWithImage extends ListActivity {
         currentUser = userList.getCurrentUser();
 
         resultList = uc.convertItemToString(currentUser, resultList);
+        adapter = new ArrayAdapter<User>(activity, R.layout.list_item_image, (ArrayList) resultList);
 
-        adapter = new ArrayAdapter<String>(activity, R.layout.list_item_image, R.id.itemName, resultList);
+        Button addItem = (Button)findViewById(R.id.btn_add_item_image);
+        addItem.setOnClickListener(new AdapterView.OnClickListener() {
+            public void onClick(View view) {
+                uc.classIntent(AddInventoryItem.class, activity);
+            }
+        });
 
+        itemList = (ListView)findViewById(R.id.lv_user_inventory_image);
+        itemList.setAdapter(adapter);
+        itemList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                uc.passValueToActivity(ItemProfile.class, activity, position);
+            }
+        });
+        itemList.setLongClickable(true);
+        itemList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            public boolean onItemLongClick(AdapterView<?> parent, View v, int position, long id) {
+                uc.passValueToActivity(EditSingleItem.class, activity, position);
+                adapter.notifyDataSetChanged();
+                return true;
+            }
+        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         resultList = uc.convertItemToString(currentUser, resultList);
+
         adapter.notifyDataSetChanged();
     }
 
